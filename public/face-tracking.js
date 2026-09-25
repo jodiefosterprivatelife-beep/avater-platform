d03042ad17a4d5ca71c54d263c9168ffbfe890ef
FM Computers <your-email@example.com>
2026-09-25 09:14:18 +0100

    Restore working movement

--- public/face-tracking.js
+++ public/face-tracking.js
@@ -2,15 +2,14 @@ console.log("Face Tracking Loaded");
 
 window.addEventListener("load", () => {
 
-    if (!window.avatarImage) return;
-
     document.addEventListener("mousemove", (event) => {
 
-        const x =
-            (event.clientX - window.innerWidth / 2) / 40;
+        if (!window.avatarImage) {
+            return;
+        }
 
-        const y =
-            (event.clientY - window.innerHeight / 2) / 40;
+        const x = (event.clientX - window.innerWidth / 2) / 40;
+        const y = (event.clientY - window.innerHeight / 2) / 40;
 
         window.avatarImage.style.transform =
             `translate(${x}px, ${y}px)`;
